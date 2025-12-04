9666fdd476c3b6bc76bad33aab01f6fc24911d49
Alvaro <a.gutierrez.ayestarain@gmail.com>
2025-12-04 22:56:55 +0100

    SSR completo

--- src/app/pages/price/pricing-page.component.ts
+++ src/app/pages/price/pricing-page.component.ts
@@ -1,8 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   imports: [],
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPage { }
+export default class PricingPage implements OnInit {
+
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+
+  ngOnInit(): void {
+
+  //   // this.title.setTitle('Pricing page');
+    // if(!isPlatformServer(this.platform))
+    // {
+    //   document.title = 'Pricing page';
+    // }
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({ name: 'description', content: 'Este es mi Pricing Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
+
+   }
+
+}
